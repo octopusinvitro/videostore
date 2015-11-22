eacc2b178f85c354852e77529f18f707d3e118f7
private <user@example.com>
2015-11-22 23:50:09 +0000

    Modify the method that gets the renter points in the new releases only and simplify it in the Price class.

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -4,11 +4,7 @@ public abstract class Price {
 
     abstract double getCharge(int daysRented);
 
-    public int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
-            return 2;
-        }
-
+    int getFrequentRenterPoints(int daysRented) {
         return 1;
     }
 }
